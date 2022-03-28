a747592f0ac98fca9279cce92775903652d94309
Artur Bas <artur.bas@hotmail.com>
2022-03-28 02:45:51 +0200

    Add comments to 21-mystery-organism project

--- codecademy-projects/full-stack-engineer-training-path/21-mystery-organism.js
+++ codecademy-projects/full-stack-engineer-training-path/21-mystery-organism.js
@@ -1,3 +1,11 @@
+/*
+PROJECT GOALS
+Context: You’re part of a research team that has found a new mysterious organism at the bottom of the ocean near hydrothermal vents. Your team names the organism, Pila aequor (P. aequor), and finds that it is only comprised of 15 DNA bases. The small DNA samples and frequency at which it mutates due to the hydrothermal vents make P. aequor an interesting specimen to study. However, P. aequor cannot survive above sea level and locating P. aequor in the deep sea is difficult and expensive. Your job is to create objects that simulate the DNA of P. aequor for your research team to study.
+
+As you progress through the steps, use the terminal and console.log() statements to check the output of your loops and functions.
+*/
+
+//SOLUTION
 // Returns a random DNA base
 const returnRandBase = () => {
   const dnaBases = ['A', 'T', 'C', 'G'];
@@ -13,10 +21,16 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+/*Create a factory function pAequorFactory() that has two parameters:
+  - number (no two organisms should have the same number)
+  - array of 15 DNA bases
+and returns an object that contains the properties specimenNum and dna that correspond to the parameters provided
+*/
 const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum: specimenNum,
     dna: dna,
+    //Add the method .mutate() that is responsible for randomly selecting a base in the object’s dna property and changing the current base to a different base. Then .mutate() will return the object’s dna.
     mutate() {
       let tempBase;
       let selectedIndex = Math.floor(Math.random() * dna.length);
@@ -26,6 +40,7 @@ const pAequorFactory = (specimenNum, dna) => {
       dna[selectedIndex] = tempBase;
       return dna;
     },
+    //Add the method .compareDNA() that has one parameter (another pAequor object), the method behavior is to compare the current pAequor‘s .dna with the passed in pAequor‘s .dna and compute how many bases are identical and in the same locations. The method does not return anything, but prints a message that states the percentage of DNA the two objects have in common — use the .specimenNum to identify which pAequor objects are being compared.
     compareDNA(pAequor) {
       let counter1 = 0;  
       for(let j = 0; j < dna.length; j++) {
@@ -35,6 +50,7 @@ const pAequorFactory = (specimenNum, dna) => {
       }
       console.log(`${specimenNum} and ${pAequor.specimenNum} have ${Math.floor(counter1 / dna.length * 100)}% in common.`)
     },
+    //Add the method .willLikelySurvive() that returns true if the object’s .dna array contains at least 60% 'C' or 'G' bases. Otherwise, the method returns false.
     willLikelySurvive() {
       let counter2 = 0;
       for(let k = 0; k < dna.length; k++) {
@@ -44,6 +60,7 @@ const pAequorFactory = (specimenNum, dna) => {
       }
       return counter2 / dna.length >= 0.6;  
     },
+    //Create the .complementStrand() method that returns the complementary DNA strand. The rules are that 'A's match with 'T's and vice versa. Also, 'C's match with 'G's and vice versa.
     complementStrand() {
       for(let l = 0; l < dna.length; l++) {
         switch(dna[l]) {
@@ -58,6 +75,7 @@ const pAequorFactory = (specimenNum, dna) => {
   };
 };
 
+//Create 30 instances of pAequor that can survive in their natural environment. Store these instances in an array.
 let pAequorWorkBase = [];
 let counter3 = 1;
 
@@ -69,6 +87,7 @@ do {
   }
 } while (pAequorWorkBase.length < 30);
 
+//Use the .compareDNA() to find the two most related instances of pAequor.
 const mostRelatedInstances = (pAequorBase) => {
   let relationsArr = [];
   for(let m = 0; m < (pAequorBase.length - 1); m++) {
@@ -76,6 +95,7 @@ const mostRelatedInstances = (pAequorBase) => {
       relationsArr.push({
         instance1: pAequorBase[m].specimenNum,
         instance2: pAequorBase[n].specimenNum,
+        //The modified .compareDNA() function 
         relationPower: function() {
           let counter4 = 0;
           for(let o = 0; o < 15; o++) {
