dd7e48fc6844c8e354df061142a1f38ce7ab5ec3
Artur Bas <artur.bas@hotmail.com>
2022-04-03 01:57:49 +0200

    Add 01-bloc-letters project

---
+++ codecademy-projects/learn-c++-training/01-block-letters/initials.cpp
@@ -0,0 +1,17 @@
+/*
+GOAL:
+Write a C++ program called initials.cpp that displays the initials of your name in block letters as shown: https://content.codecademy.com/courses/learn-cpp/hello-world/block-letters.png
+*/
+
+//SOLUTION:
+#include <iostream>
+
+int main () {
+  std::cout << "  A    BBBB \n";
+  std::cout << " A A   B   B\n";
+  std::cout << "A   A  B   B\n";
+  std::cout << "AAAAA  BBBB \n";
+  std::cout << "A   A  B   B\n";
+  std::cout << "A   A  B   B\n";
+  std::cout << "A   A  BBBB \n"; 
+}
